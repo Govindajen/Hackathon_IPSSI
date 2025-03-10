d4ba7ee112e5445979e26cb9626ff119e725ecb3
Govindarajen Coodien <coodien21@gmail.com>
2025-03-10 23:07:04 +0100

    fix login

--- front-hackathon/src/pages/Authentication/index.jsx
+++ front-hackathon/src/pages/Authentication/index.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { login, register } from "../../redux/slices/authSlice";
+import { useDispatch } from "react-redux";
 
 export default function Authentication () {
+    const dispatch = useDispatch();
 
     const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
@@ -11,53 +13,14 @@ export default function Authentication () {
 
     function handleLogin ({username, password}) {
 
-        login({username, password})
+        dispatch(login({username, password}))
         
     }
 
     function handleRegister ({email, username, password}) {
         
-        register({email, username, password})
+        dispatch(register({email, username, password}))
     }
-
-    const SignIn = () => {
-
-        return (
-            <div className="loginContainer">
-                <span>
-                    <p className="label-input">Username</p>
-                    <input className="input-primary" type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} value={username}/>
-                </span>
-                <span>
-                    <p className="label-input">Password</p>
-                    <input className="input-primary" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
-                </span>
-                <button className="btn-primary loginBtn" onClick={() => handleLogin(username, password)}>Connexion</button>
-            </div>
-        )
-    }
-
-    const SignUp = () => {
-
-        return (
-            <div className="loginContainer">
-                <span>
-                    <p className="label-input">Email</p>
-                    <input className="input-primary" type="text" placeholder="Username" onChange={(e) => setEmail(e.target.value)} value={email}/>
-                </span>
-                <span>
-                    <p className="label-input">Username</p>
-                    <input className="input-primary" type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} value={username}/>
-                </span>
-                <span>
-                    <p className="label-input">Password</p>
-                    <input className="input-primary" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
-                </span>
-                <button className="btn-primary loginBtn" onClick={() => {handleRegister(email, username, password)}}>Enregistrer</button>
-            </div>
-        )
-    }
-
 
 
     return (
@@ -70,7 +33,35 @@ export default function Authentication () {
                     </div>
                     <div className="rightArea">
 
-                        {page ? <SignIn /> : <SignUp />}
+                        {page ? (
+                                <div className="loginContainer">
+                                <span>
+                                    <p className="label-input">Username</p>
+                                    <input className="input-primary" type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} value={username}/>
+                                </span>
+                                <span>
+                                    <p className="label-input">Password</p>
+                                    <input className="input-primary" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
+                                </span>
+                                <button className="btn-primary loginBtn" onClick={() => {handleLogin({username, password})}}>Connexion</button>
+                            </div>
+                        ) : (
+                            <div className="loginContainer">
+                            <span>
+                                <p className="label-input">Email</p>
+                                <input className="input-primary" type="text" placeholder="Username" onChange={(e) => setEmail(e.target.value)} value={email}/>
+                            </span>
+                            <span>
+                                <p className="label-input">Username</p>
+                                <input className="input-primary" type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} value={username}/>
+                            </span>
+                            <span>
+                                <p className="label-input">Password</p>
+                                <input className="input-primary" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
+                            </span>
+                            <button className="btn-primary loginBtn" onClick={() => {handleRegister({email, username, password})}}>Enregistrer</button>
+                        </div>
+                        )}
                         <button className="btn-primary" onClick={() => {setPage(!page)}}>
                             {page ? "Vous n'avez pas de compte?" : "Vous avez déjà un compte?"}
                         </button>
