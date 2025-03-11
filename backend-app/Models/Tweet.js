0c82d0f9740f3b4ca987ec59e86cfc686de7add3
Kvn9 <kevintata02@gmail.com>
2025-03-11 14:34:42 +0100

    Style

--- backend-app/Models/Tweet.js
+++ backend-app/Models/Tweet.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 const TweetSchema = new mongoose.Schema({
     content: { type: String, required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
-    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
-    retweets: { type: mongoose.Schema.Types.ObjectId, ref: "tweets"},
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    retweets: { type: mongoose.Schema.Types.ObjectId, ref: "Tweet"},
     hashtags: [{ type: String }],
-    commentaire: [{ type: mongoose.Schema.Types.ObjectId, ref: "tweets" }],
-    signet: { type: mongoose.Schema.Types.ObjectId, ref: "tweets" },
+    commentaire: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tweet" }],
+    signet: { type: mongoose.Schema.Types.ObjectId, ref: "Tweet" },
     date: { type: Date, default: Date.now },
 }, { timestamps: true });
 
